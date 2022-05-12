551d1523a4ec27c6abd0e5239b55da273aa08251
Sebastien Bourgeois <sebastienbourgeois60@gmail.com>
2022-05-12 21:29:18 +0200

    Réorganisation du répertoire du projet en vue du déploiement de l'API et du dashboard

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: cd code && python deploiement_modele.py
-web: streamlit run dashboard.py
--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-cd code
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
